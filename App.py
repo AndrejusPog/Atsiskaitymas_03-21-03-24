92bce045f01bfc483577bfc57aaa039b09496740
Andrejus <andrejuspog@gmail.com>
2023-03-21 23:30:48 +0200

    reg and login works, need to figue out how flash works and create log off method

---
+++ App.py
@@ -0,0 +1,76 @@
+import os
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
+from flask_sqlalchemy import SQLAlchemy
+
+import forms
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Za2sd12dASasd21dwasddASDAd21'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'user.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = "Login"
+login_manager.login_message_category = "Information"
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=True, nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    db.create_all()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        encrypted_pass = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(name=form.name.data, email=form.email.data,
+                    password=encrypted_pass)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('groups'))
+        else:
+            flash('Please check your email and password')
+    return render_template("login.html", form=form)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(host='127.0.0.1', port=8000, debug=True)
