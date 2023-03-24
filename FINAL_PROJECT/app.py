227919adba1149c4acb4e57d7e8beeed6103fa29
Andrejus <andrejuspog@gmail.com>
2023-03-24 12:29:26 +0200

    some code was missing. lol

--- FINAL_PROJECT/app.py
+++ FINAL_PROJECT/app.py
@@ -11,7 +11,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfgsfdgsdfgsdfgsdf'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'myBills.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = Falses
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 bcrypt = Bcrypt(app)
